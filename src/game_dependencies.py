033a0c17897acdaa607580511d4995aebf579761
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-26 13:17:06 +0530

    adding logic for level
    This will allow the code to increase the level which is basically the speed of bombs based upon the score of the player

--- src/game_dependencies.py
+++ src/game_dependencies.py
@@ -1,25 +1,48 @@
 import random
 import pygame
-import sys
 
-def drop_bombs(bomb_list):
-    if len(bomb_list) < 10:
-        x_pos = random.randint(0, width - 50)
+
+def lvl(score, bomb_speed):
+    if score < 10:
+        bomb_speed = 3
+    elif score < 20:
+        bomb_speed += 1
+    elif score < 30:
+        bomb_speed += 1
+    elif score < 40:
+        bomb_speed += 1
+
+    return bomb_speed
+
+
+def drop_bombs(bomb_list, width):
+    randomize_fall = random.random()
+    if len(bomb_list) < 10 and randomize_fall < 0.2:
+        x_pos = random.randint(0, width-50)
         y_pos = 0
         bomb_list.append([x_pos, y_pos])
 
 
-def draw_bombs(bomb_list):
+def draw_bombs(bomb_list, screen):
     for bomb_pos in bomb_list:
         pygame.draw.rect(screen, (0, 0, 255), (bomb_pos[0], bomb_pos[1], 50, 50))
 
 
-def bomb_pos_inc(bomb_list):
-    for bomb_pos in bomb_list:
+def bomb_pos_inc(bomb_list, scr, height, bomb_speed):
+    for index, bomb_pos in enumerate(bomb_list):
         if 0 <= bomb_pos[1] < height:
             bomb_pos[1] += bomb_speed
         else:
             bomb_list.pop(index)
+            scr += 1
+    return scr
+
+
+def hit_check(player_pos, bomb_list):
+    for bomb_pos in bomb_list:
+        if hit_by_bomb(player_pos, bomb_pos):
+            return True
+    return False
 
 
 def hit_by_bomb(p_pos, b_pos):
@@ -29,4 +52,5 @@ def hit_by_bomb(p_pos, b_pos):
     if (b_x >= p_x and b_x < (p_x + 50)) or (p_x >= b_x and p_x < (b_x + 50)):
         if (b_y >= p_y and b_y < (p_y + 50)) or (p_y >= b_y and p_y < (b_y + 50)):
             return True
+
     return False
